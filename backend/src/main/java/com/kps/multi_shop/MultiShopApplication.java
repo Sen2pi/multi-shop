5c3d1617eaf3fdb454f5b3b3390d3dba49dc2011
Karim Patatas <dr.karim.patatas@gmail.com>
2025-07-31 02:50:41 +0100

    backend spring start

---
+++ backend/src/main/java/com/kps/multi_shop/MultiShopApplication.java
@@ -0,0 +1,13 @@
+package com.kps.multi_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiShopApplication.class, args);
+	}
+
+}
